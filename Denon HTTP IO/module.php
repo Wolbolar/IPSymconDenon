17056d615ddf3551e7a0d41b83f11407a4bcb653
Wolbolar <wolbolar@gmail.com>
2016-03-25 16:56:14 +0100

    RegisterTimer I/O HTTP

--- Denon HTTP IO/module.php
+++ Denon HTTP IO/module.php
@@ -56,7 +56,39 @@ class DenonAVRIOHTTP extends IPSModule
 	$this->RegisterTimer('Update', $this->ReadPropertyString('UpdateInterval'), 'DAVRIO_GetStatus($id)');
 	}	
 
+	protected function RegisterTimer($ident, $interval, $script) {
+		$id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
 
+		if ($id && IPS_GetEvent($id)['EventType'] <> 1)
+		{
+		  IPS_DeleteEvent($id);
+		  $id = 0;
+		}
+
+		if (!$id)
+		{
+		  $id = IPS_CreateEvent(1);
+		  IPS_SetParent($id, $this->InstanceID);
+		  IPS_SetIdent($id, $ident);
+		}
+
+		IPS_SetName($id, $ident);
+		IPS_SetHidden($id, true);
+		IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
+
+		if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
+
+		if (!($interval > 0))
+		{
+		  IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+		  IPS_SetEventActive($id, false);
+		}
+		else
+		{
+		  IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $interval);
+		  IPS_SetEventActive($id, true);
+		}
+	  }
 
 ################## Datapoints
  
