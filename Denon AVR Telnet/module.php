2ed8395d6b5115dbf8e64084460d2ffd472ef424
Wolbolar <wolbolar@gmail.com>
2016-04-27 11:09:54 +0200

    Denon Telnet Modul
    Bugfix Zone2 Zone3 Name

--- Denon AVR Telnet/module.php
+++ Denon AVR Telnet/module.php
@@ -1013,7 +1013,7 @@ class DenonAVRTelnet extends IPSModule
 				{ 
 					if ($ResponseType == "HTTP")
 					{
-						if ($Ident == "MainZoneName" || $Ident == "Model")
+						if ($Ident == "MainZoneName" || $Ident == "Zone2Name" || $Ident == "Zone3Name" || $Ident == "Model")
 						{
 							switch ($VarType)
 							{
