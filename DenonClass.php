c743bcab3c8fb8519d0dd834320337559612ba5f
bumaas <burkhard.maas@arcor.de>
2017-08-19 20:36:14 +0200

    Beta 28
    korrigiert:
    - Marantz InputSource 'Online Music' hinzugefügt

--- DenonClass.php
+++ DenonClass.php
@@ -2465,8 +2465,10 @@ class DENONIPSProfiles extends stdClass
         $countinput = count($InputFuncList[0]->value);
 
         for ($i = 0; $i <= $countinput-1; $i++){
-            if ((string)$SourceDelete[0]->value[$i] == "USE")
-            {
+            if ($this->debug){
+                IPS_LogMessage(get_class().'::'.__FUNCTION__, '$SourceDelete['.$i.']: '.json_encode($SourceDelete[0]->value[$i]));
+            }
+            if ((string)$SourceDelete[0]->value[$i] == "USE" || is_null($SourceDelete[0]->value[$i])){
                 $UsedInput_i++;
                 $MinValue[$UsedInput_i] = $UsedInput_i;
                 if ($MainForm == DENON_HTTP_Interface::MainForm_old){
